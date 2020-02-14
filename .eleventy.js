a098ffc12e0afbf214793b290705524dc5eabbae
Raymond Camden <raymondcamden@gmail.com>
2020-02-14 14:38:55 -0600

    more work

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
 module.exports = function(eleventyConfig) {
 
+	eleventyConfig.addPassthroughCopy("css");
+	eleventyConfig.addPassthroughCopy("js");
+	eleventyConfig.addPassthroughCopy("images");
+	eleventyConfig.addPassthroughCopy("fonts");
+
 	eleventyConfig.addCollection("posts", collection => {
-		return collection.getFilteredByGlob("_posts/**/*.md");
+		let foo = collection.getFilteredByGlob("_posts/**/*.md");
+		//console.log(foo[0]);
+		for(let i=0;i<foo.length;i++) {
+			foo[i].data.permalink += '.html';
+			foo[i].outputPath += '/index.html';
+		}
+
+		return foo;
+//		return collection.getFilteredByGlob("_posts/**/*.md");
 	});
 }
---
+++ js/custom.js
@@ -0,0 +1,37 @@
+/**
+ * Main JS file for BlogInn behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+        // Responsive video embeds
+        $('.entry-content').fitVids();
+
+        // Navigation
+        $('#menu-toggle').click(function(){
+            var _this = $(this);
+            _this.toggleClass( 'toggled-on' ).attr('aria-expanded', _this.attr('aria-expanded') === 'false' ? 'true' : 'false');
+            $('.nav-menu').slideToggle();
+        });
+        $(window).bind('resize orientationchange', function() {
+            if ( $('#menu-toggle').is(':hidden') ) {
+                $('#menu-toggle').removeClass('toggled-on').attr('aria-expanded', 'false');
+                $('.nav-menu').removeAttr('style');
+            }
+        });
+
+        // Scroll to top
+        $('#top-link').on('click', function(e) {
+            $('html, body').animate({'scrollTop': 0});
+            e.preventDefault();
+        });
+
+        // recent posts
+        $('.recent-posts').load('/recentPosts/');
+
+    });
+
+}(jQuery));
