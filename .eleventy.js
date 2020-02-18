9b89eb344f9b0c44842fc04343fce6c750053ef4
Raymond Camden <raymondcamden@gmail.com>
2020-02-18 06:37:12 -0600

    updates

--- .eleventy.js
+++ .eleventy.js
@@ -5,15 +5,71 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("images");
 	eleventyConfig.addPassthroughCopy("fonts");
 
+	//reference: https://github.com/11ty/eleventy/issues/179#issuecomment-413119342
+	eleventyConfig.addShortcode('excerpt', post => extractExcerpt(post));
+
+	// reverse isn't supported in 11 liquid?
+	eleventyConfig.addFilter("reverse", a => a.slice().reverse() );
+
 	eleventyConfig.addCollection("posts", collection => {
-		let foo = collection.getFilteredByGlob("_posts/**/*.md");
-		//console.log(foo[0]);
-		for(let i=0;i<foo.length;i++) {
-			foo[i].data.permalink += '.html';
-			foo[i].outputPath += '/index.html';
+		let posts = collection.getFilteredByGlob("_posts/**/*.md");
+		for(let i=0;i<posts.length;i++) {
+			posts[i].data.permalink += '.html';
+			posts[i].outputPath += '/index.html';
 		}
 
-		return foo;
-//		return collection.getFilteredByGlob("_posts/**/*.md");
+		return posts;
 	});
+
+
+};
+
+const excerptMinimumLength = 140;
+const excerptSeparator = '<!--more-->';
+
+/**
+ * Extracts the excerpt from a document.
+ *
+ * @param {*} doc A real big object full of all sorts of information about a document.
+ * @returns {String} the excerpt.
+ */
+function extractExcerpt(doc) {
+  if (!doc.hasOwnProperty('templateContent')) {
+    console.warn('Failed to extract excerpt: Document has no property `templateContent`.');
+    return;
+  }
+
+  const content = doc.templateContent;
+  if (content.includes(excerptSeparator)) {
+    return content.substring(0, content.indexOf(excerptSeparator)).trim();
+  }
+  else if (content.length <= excerptMinimumLength) {
+    return content.trim();
+  }
+
+  const excerptEnd = findExcerptEnd(content);
+  return content.substring(0, excerptEnd).trim();
+}
+
+/**
+ * Finds the end position of the excerpt of a given piece of content.
+ * This should only be used when there is no excerpt marker in the content (e.g. no `<!--more-->`).
+ *
+ * @param {String} content The full text of a piece of content (e.g. a blog post)
+ * @param {Number?} skipLength Amount of characters to skip before starting to look for a `</p>`
+ * tag. This is used when calling this method recursively.
+ * @returns {Number} the end position of the excerpt
+ */
+function findExcerptEnd(content, skipLength = 0) {
+  if (content === '') {
+    return 0;
+  }
+
+  const paragraphEnd = content.indexOf('</p>', skipLength) + 4;
+
+  if (paragraphEnd < excerptMinimumLength) {
+    return paragraphEnd + findExcerptEnd(content.substring(paragraphEnd), paragraphEnd);
+  }
+
+  return paragraphEnd;
 }
