c8b57a9bb6980cb36054a804c317bd15574c4066
Raymond Camden <raymondcamden@gmail.com>
2020-03-07 11:00:54 -0600

    add ads.txt

--- .eleventy.js
+++ .eleventy.js
@@ -11,6 +11,7 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./manifest.json");
 	eleventyConfig.addPassthroughCopy("./favicon.ico");
 	eleventyConfig.addPassthroughCopy("./robots.txt");
+	eleventyConfig.addPassthroughCopy("./ads.txt");
 	eleventyConfig.addPassthroughCopy("./_redirects");
 	eleventyConfig.addPassthroughCopy("./*.png");
 
