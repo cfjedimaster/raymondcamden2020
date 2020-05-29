c1d6f90684255392aeee9baea719110f3ead1aa8
Raymond Camden <raymondcamden@gmail.com>
2020-05-29 10:16:39 -0500

    functions 5

--- .functions/deploy-succeeded/deploy-succeeded.js
+++ .functions/deploy-succeeded/deploy-succeeded.js
@@ -1,9 +1,38 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const SG_KEY = process.env.SENDGRID;
+
+const helper = require('sendgrid').mail;
+
 exports.handler = async (event, context) => {
   try {
-    console.log('build result');
-    console.log(JSON.stringify(event.body, null, '\t'));
+
+    console.log('deploy succeeded run!');
+    let pubData = JSON.parse(event.body).payload;
+    let body = `
+Deploy Succeeded for ${pubData.name}
+
+    `;
+
+    sendEmail(body, 'Netlify Build Succeeded', 'raymondcamden@gmail.com', 'raymondcamden@gmail.com');
+    
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
+}
+
+function sendEmail(body, subject, from_email, to_email) {
+  let mailContent = new helper.Content('text/plain', body);
+  let mail = new helper.Mail(from_email, subject, to_email, mailContent);
+  let sg = require('sendgrid')(SG_KEY);
+
+  let request = sg.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+
+  sg.API(request, function(error, response) {
+    if(error) {
+    console.log(error.response.body);
+    }
+  });
 }
