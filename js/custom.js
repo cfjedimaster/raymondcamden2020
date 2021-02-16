2414badd7cf2613f2e79cb628e1fa453fde67260
Raymond Camden <raymondcamden@gmail.com>
2021-02-16 09:51:24 -0600

    webmention support

--- js/custom.js
+++ js/custom.js
@@ -30,7 +30,10 @@
         });
 
         //blog posts only, cheap way to do this
-        if(window.location.pathname.indexOf('/2') === 0) $('.entry-content > p:nth-of-type(3)').after('<div id="ezoic-pub-ad-placeholder-117"></div>');
+        if(window.location.pathname.indexOf('/2') === 0) {
+            $('.entry-content > p:nth-of-type(3)').after('<div id="ezoic-pub-ad-placeholder-117"></div>');
+            doWebMentions();
+        }
 
         window.cookieconsent.initialise({
             "palette": {
@@ -94,4 +97,100 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js');
   });
+}
+
+async function doWebMentions() {
+    console.log('do WebMentions');
+    let url = 'https://www.raymondcamden.com' + document.location.pathname.substring(0, document.location.pathname.length-1);
+    let mentions = await getWebmentions(url);
+    console.log('mentions', mentions.length);
+
+    let likes = mentions.filter(m => m['wm-property'] === 'like-of');
+    console.log('Likes', likes.length);
+
+    let retweets = mentions.filter(m => m['wm-property'] === 'repost-of');
+    console.log('RTs', retweets.length);
+
+    let replies = mentions.filter(m => ['in-reply-to','mention-of'].indexOf(m['wm-property']) >= 0);
+    console.log('Replies', replies.length);
+
+    if(replies.length) {
+        let title = `<h3>${replies.length} ${replies.length !== 1?'Replies':'Reply'}</h3>`;
+        let html = title;
+
+        replies.forEach(m => {
+            let iMention = `
+            <div class="comment"">
+                <div>
+                    <img src="${m.author.photo?m.author.photo:'/images/blankavatar.gif'}" width="48" height="48" class="lazyload" alt="${m.author.name}">
+                </div>
+                <div>
+                    <div class="comment_header">
+                    Reply by ${m.author.name}
+                    posted on ${dateFormat(m.published)}
+                    </div>
+                    <div class="comment_text">${truncate(m.content.text, 300)}</div>
+                    <a href="$(m.url}" target="_blank" rel="noopener noreferrer">
+                    source <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="10px"><path fill="currentColor" d="M432 320h-32a16 16 0 00-16 16v112H64V128h144a16 16 0 0016-16V80a16 16 0 00-16-16H48a48 48 0 00-48 48v352a48 48 0 0048 48h352a48 48 0 0048-48V336a16 16 0 00-16-16zM488 0H360c-21.37 0-32.05 25.91-17 41l35.73 35.73L135 320.37a24 24 0 000 34L157.67 377a24 24 0 0034 0l243.61-243.68L471 169c15 15 41 4.5 41-17V24a24 24 0 00-24-24z" /></svg>
+                    </a>
+                </div>
+            </div>
+            `;
+            html += iMention;
+        });
+        $('#webmentions-replies').html(html);
+
+    }
+
+    if(likes.length || retweets.length) {
+        let title = `<h3>${likes.length} Like${likes.length > 1?'s':''} and ${retweets.length} Retweet${retweets.length > 1?'s':''}</h3>`;
+        let html = title;
+
+        if(likes.length) {
+            likes.forEach(l => {
+                let iLike = '';
+                if(l.author.photo) iLike = `<img src="${l.author.photo}" alt="${l.author.name}" width="48" height="48" class="lazyload">`;
+                else iLike = `<img src="/images/blankavatar.gif" width="48" height="48" class="lazyload" alt="${l.author.name}">`;
+
+                if(l.url) iLike = `<a href="${l.url}" target="_blank" rel="noopener noreferrer">` + iLike + '</a>';
+                html += iLike;
+            });
+        }
+
+        if(retweets.length) {
+            let rtHTML = `<h3>${retweets.length} Retweet` + (retweets.length > 1?'s':'') + '</h3>';
+            retweets.forEach(r => {
+                let iRT = '';
+                if(r.author.photo) iRT = `<img src="${r.author.photo}" alt="${r.author.name}" width="48" height="48" class="lazyload">`;
+                else iRT = `<img src="/images/blankavatar.gif" width="48" height="48" class="lazyload" alt="${r.author.name}">`;
+
+                if(r.url) iRT = `<a href="${r.url}" target="_blank" rel="noopener noreferrer">` + iRT + '</a>';
+                html += iRT;
+            });
+        }
+
+        $('#webmentions-likes').html(html);
+
+    }
+
+}
+
+function truncate(s, len) {
+    if(s.length < len) return s;
+    else return s.substring(0,len-3)+'...';
+}
+
+function dateFormat(d) {
+    if(!Intl || !Intl.DateTimeFormat) return d;
+	d = new Date(d);
+	return new Intl.DateTimeFormat().format(d) + ' at ' + new Intl.DateTimeFormat('en-US', {hour:'numeric',minute:'2-digit'}).format(d);
+}
+
+async function getWebmentions(url) {
+ return new Promise((resolve, reject) => {
+   let api = `https://webmention.io/api/mentions.jf2?target=${url}&per-page=200`;
+   fetch(api)
+   .then(res => res.json())
+   .then(res => resolve(res.children));
+ }); 
 }
