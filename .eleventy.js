5ba4d037c5055399a9222c18689e841b67112883
Raymond Camden <raymondcamden@gmail.com>
2022-11-21 11:00:19 -0600

    tweak for eleventy serve

--- .eleventy.js
+++ .eleventy.js
@@ -19,6 +19,8 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./*.png");
 	eleventyConfig.addPassthroughCopy("./service-worker.js");
 
+  eleventyConfig.setServerPassthroughCopyBehavior("copy");
+  
   eleventyConfig.setQuietMode(true);
   eleventyConfig.addPlugin(directoryOutputPlugin);
   
